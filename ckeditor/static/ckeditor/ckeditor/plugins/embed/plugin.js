3aef30f047c37fa3e3abbfb027412e171b3d288b
Gil Brechbuhler <gibre12@gmail.com>
2017-06-12 16:47:21 +0200

    noembed does not work, try twitter oembed directly

--- ckeditor/static/ckeditor/ckeditor/plugins/embed/plugin.js
+++ ckeditor/static/ckeditor/ckeditor/plugins/embed/plugin.js
@@ -23,7 +23,7 @@
 				requiredContent: 'div[data-oembed-url]',
 				providerUrl: new CKEDITOR.template(
 					editor.config.embed_provider ||
-					'//noembed.com/embed?url={url}&callback={callback}'
+					'//publish.twitter.com/oembed?url={url}&callback={callback}'
 				),
 
 				// The filter element callback actually allows all divs with data-oembed-url,
