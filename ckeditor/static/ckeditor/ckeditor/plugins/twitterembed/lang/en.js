0cf77417c5d1d8c1607024d5d51c407aa5d13427
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2017-06-30 09:16:01 +0200

    declare plugin language with correct plugin name

--- ckeditor/static/ckeditor/ckeditor/plugins/twitterembed/lang/en.js
+++ ckeditor/static/ckeditor/ckeditor/plugins/twitterembed/lang/en.js
@@ -2,7 +2,7 @@
 Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'embedbase', 'en', {
+CKEDITOR.plugins.setLang( 'twitterembed', 'en', {
 	pathName: 'media object',
 	title: 'Twitter Embed',
 	button: 'Insert Twitter Embed',
