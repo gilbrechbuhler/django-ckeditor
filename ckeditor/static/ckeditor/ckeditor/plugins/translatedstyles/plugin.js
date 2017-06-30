2e60bcff814b881a837bdcca4b156c03b7e48438
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2017-06-30 10:39:22 +0200

    Revert "Revert "little test for framed text translation""
    This reverts commit f7ff28486512c69354e0de271f865e57ddc0fbc0.

---
+++ ckeditor/static/ckeditor/ckeditor/plugins/translatedstyles/plugin.js
@@ -0,0 +1,17 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+( function() {
+	'use strict';
+
+	CKEDITOR.plugins.add( 'translatedstyles', {
+		lang: 'en,fr',
+
+		init: function( editor ) {
+			console.log(CKEditor.stylesSet.get('default'))
+		}
+	} );
+
+} )();
